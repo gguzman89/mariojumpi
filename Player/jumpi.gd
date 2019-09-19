dc3e8d6e8cb6388780165eb6e812cbab3e813fa0
gguzman89 <ggtpi0gabriel@gmail.com>
2019-09-19 01:01:42 -0300

    fix just_press

--- Player/jumpi.gd
+++ Player/jumpi.gd
@@ -2,22 +2,20 @@ extends KinematicBody2D
 
 const ACCEL = 1500
 const MAX_SPEED = 500
-const FRICTION = -500
+const FRICTION = -500 #
 
-const GRAVITY = 2000
-#export (int) var JUMP_SPEED = 5000
+const GRAVITY = 4000
+const JUMP_SPEED = -1500
 
 var acc = Vector2()
 var vel = Vector2()
 
 func _ready():
 	set_physics_process(true)
-	#pass
 	
 func _physics_process(delta):
-	#acc.x = Vector2.ZERO.x
 	acc.y = GRAVITY
-	
+		
 	if Input.is_action_pressed("ui_right"):
 		acc.x = Vector2.RIGHT.x
 	elif Input.is_action_pressed("ui_left"):
@@ -29,8 +27,10 @@ func _physics_process(delta):
 	if acc.x == 0:
 		acc.x = vel.x * FRICTION * delta
 	
+	if Input.is_action_pressed("ui_up") and is_on_floor():
+		vel.y = JUMP_SPEED
+	
 	vel += acc * delta
-	# vel.y += acc.y 
 	vel.x = clamp(vel.x, -MAX_SPEED, MAX_SPEED)
 	vel = move_and_slide(vel, Vector2.UP)
 	
